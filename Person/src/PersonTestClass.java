4d90ee9f7d5e01f7aab84f184267b715807ad217
manasabyndoor <41484448+manasabyndoor@users.noreply.github.com>
2018-08-04 10:37:03 +0530

    Add files via upload

---
+++ Person/src/PersonTestClass.java
@@ -0,0 +1,13 @@
+import java.sql.Date;
+
+public class PersonTestClass {
+	public static void main(String args[])
+	{
+		Person Ram=new Person(null, 0, 0, 0);
+		
+		int year;
+		int month;
+		Date date=new Date(int year,int month,int date);
+	}
+
+}
